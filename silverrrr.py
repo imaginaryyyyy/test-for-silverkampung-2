2b7260a432c25750e8d05bdbe844ebc5273777a3
imaginaryyyyy <imaginary.biz@gmail.com>
2026-08-18 18:05:46 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -1,43 +1,36 @@
-import streamlit as st
-import datetime as dt
-
-#Kaiser's function to read a .json file
-def ReadFromJson(fp, *locations):
-  with open(fp,"r") as f:
-    myFile = json.load(f)
-  return reduce(operator.getitem, locations, myFile)
-
-st.title("Silver Kampong")
-
-#e.g ReadFromJson(file.json, "movies")
-
-#Movies dictionary
+#import streamlit as st
 movies = [
         {"title": "My Children", "desc": "A man who values his children", "showtimes": "9.00 AM", "halls": "Cinema Hall 1", "photos": "https://images.pexels.com/photos/15914002/pexels-photo-15914002.jpeg", "date": "2026-08-26"},
         {"title": "My Struggle", "desc": "Is this source reliable?", "showtimes": "12.00 PM", "halls": "Cinema Hall 2", "photos": "https://images.pexels.com/photos/9804995/pexels-photo-9804995.jpeg", "date": "2026-08-27"},
         {"title": "-man", "desc": "-I am powerless", "showtimes": "3.00 PM", "halls": "Cinema Hall 3", "photos": "https://images.pexels.com/photos/28344947/pexels-photo-28344947.jpeg", "date": "2026-08-28"}]
+
+def extract(movies, key, Type):
+    Type = []
+    for movie in movies:
+        if movie[key] not in Type:
+            Type.append(movie[key])
+    return Type
+
+    
+
 
 #Setting the filters selection
 filters = st.selectbox("", options=["All", "Showtimes", "Halls", "Date"], index=0)
 
 filtered_movies = []
 
-
 if filters == "All":
     filtered_movies = movies
 
 elif filters == "Date":
-    selected_date = st.date_input("Select Date", max_value=dt.date(2026, 12, 31), format="DD/MM/YYYY")
+    selected_date = st.date_input("Select Date", min_value=dt.date(2026, 8, 1), max_value=dt.date(2026, 12, 31), format="DD/MM/YYYY")
     date_str = str(selected_date)
     for movie in movies:
         if movie["date"] == date_str:
             filtered_movies.append(movie)
 
 elif filters == "Showtimes":
-    all_times = []
-    for movie in movies:
-        if movie["showtimes"] not in all_times:
-            all_times.append(movie["showtimes"])
+    extract(movies, "showtimes", timings) 
     showtimes_filter = st.pills("", options=all_times, default=all_times, selection_mode="multi")
 
     for movie in movies:
@@ -46,10 +39,7 @@ elif filters == "Showtimes":
 
 
 elif filters == "Halls":
-    all_halls = []
-    for movie in movies:
-        if movie["halls"] not in all_halls:
-            all_halls.append(movie["halls"])
+    extract(movies, "halls", halls) 
     halls_filter = st.pills("", options=all_halls, default=all_halls, selection_mode="multi")
 
     for movie in movies:
@@ -62,18 +52,22 @@ if filtered_movies:
         movie = filtered_movies[i]
         col = cols[i]
         with col:
-            st.subheader(movie["title"])
-            st.caption(movie["desc"])
-            st.image(movie["photos"])
-            if filters != "Date":
-                st.write("Date: ")
-                st.button(movie["date"])
-            if filters != "Showtimes":
-                st.write("Showtimes: ")
-                st.button(movie["showtimes"], key=f"{movie['title']}, {movie["showtimes"]}")
-            if filters != "Halls":
-                st.write("Halls: ")
-                st.button(movie["halls"], key=f"{movie['title']}, {movie['halls']}")
+            if filters == "All":
+                st.subheader(movie["title"])
+                st.caption(movie["desc"])
+                st.image(movie["photos"])
+            elif filters != "All":
+                st.subheader(movie["title"])
+                st.caption(movie["desc"])
+                st.image(movie["photos"])
+                if filters != "Date":
+                    st.write("Date: ")
+                    st.button(movie["date"])
+                if filters != "Showtimes":
+                    st.write("Showtimes: ")
+                    st.button(movie["showtimes"], key=f"{movie['title']}, {movie["showtimes"]}")
+                if filters != "Halls":
+                    st.write("Halls: ")
+                    st.button(movie["halls"], key=f"{movie['title']}, {movie['halls']}")
 else:
-    st.info("There are no movies. Please seek help as soon as possible.")
-    st.balloons()
+    st.info("There are no movies for the option selected.")
