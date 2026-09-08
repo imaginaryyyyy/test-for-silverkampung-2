075b18ff43fdb9268fc5dbacb90b45a1315c074e
imaginaryyyyy <imaginary.biz@gmail.com>
2026-09-08 14:31:57 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -2,6 +2,7 @@ import streamlit as st
 import datetime as dt
 import google.generativeai as genai
 import json
+import random
 
 api_key = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
@@ -80,7 +81,8 @@ st.subheader("MovieFinder Assistant")
 user_input = st.chat_input("If you are lost, ask about what kind of movie you want:")
 
 if user_input:
-    prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. "
-    response = model.generate_content(prompt)
-    container = st.container(border=True)
-    container.write(response.text)
+        with st.spinner(f"MovieFinder rates you a {random.randint(1,10)}")
+                prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. "
+                response = model.generate_content(prompt)
+                container = st.container(border=True)
+                container.write(response.text)
