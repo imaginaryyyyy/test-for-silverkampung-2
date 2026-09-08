fec3a2969d6986bf3df8275f19f2026de389db08
imaginaryyyyy <imaginary.biz@gmail.com>
2026-09-08 14:38:14 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -80,8 +80,18 @@ else:
 user_input = st.chat_input("If you are lost, ask about what kind of movie you want:")
 
 if user_input:
-        with st.spinner(f"MovieFinder rates you a {random.randint(0,10)}"):
-                prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. "
+        rating = random.randint(0,10)
+        look = ""
+        if rating == 0:
+                look = "Is this physically possible?"
+        elif rating > 0 and rating < 5:
+                look = "We don't want you on this site."
+        elif rating > 4 and rating < 8:
+                look = "Looking good"
+        elif rating >= 8:
+                look = "Please don't leave."
+        with st.spinner(f"MovieFinder rates you a {rating}/10. {look}."):
+                prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. Also talk about them based on their MovieFinder rating: {rating}, {look} "
                 response = model.generate_content(prompt)
                 container = st.container(border=True)
                 container.write(response.text)
