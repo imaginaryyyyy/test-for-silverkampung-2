9d33cc37a02dccbfe862b0ec3e05e7795815b29f
imaginaryyyyy <imaginary.biz@gmail.com>
2026-09-08 14:32:54 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -77,11 +77,10 @@ if filtered_movies:
 else:
     st.info("There are no movies for the date selected.")
 
-st.subheader("MovieFinder Assistant")
 user_input = st.chat_input("If you are lost, ask about what kind of movie you want:")
 
 if user_input:
-        with st.spinner(f"MovieFinder rates you a {random.randint(1,10)}")
+        with st.spinner(f"MovieFinder rates you a {random.randint(0,10)}"):
                 prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. "
                 response = model.generate_content(prompt)
                 container = st.container(border=True)
