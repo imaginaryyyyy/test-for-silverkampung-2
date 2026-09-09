f85326e6e08e6c131dbe317662529776c31aa9a0
imaginaryyyyy <imaginary.biz@gmail.com>
2026-09-09 16:03:53 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -93,7 +93,7 @@ if user_input:
         with st.spinner(f"MovieFinder rates you a {rating}/10. {look}."):
                 container_user_prompt = st.container(border=True)
                 container_user_prompt.write(f":smile:: \n {user_input}")
-                prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. Instead of saying the YYYY/MM/DD change it to actual words"
+                prompt = f"You are a Movie Finder Assistant who's job is to recommend a movie to watch based on centext data and user needs (from their question). Context: {data_for_ai}, User Input: {user_input}. You need to sound sure to give the user confidence in what you are saying. Do not say words such as 'likely'. For reference, 'My Children' is a high-stakes documentary (thriller) on catching predators, 'My Struggle' is a drama re-enacting Hitler's rise to power and fall, '-man' is a comedy-action hybrid on a man trying to be a superhero. You should expand on ths yourself. Maintain a friendly but persuasive tone. Instead of saying the YYYY/MM/DD change it to, for example 'Augutst 26, 2026'"
                 response = model.generate_content(prompt)
                 container_ai_response = st.container(border=True)
-                container_ai_response.write(f":robot:: \n {response.text}")
+                container_ai_response.write(f":robot_face:: \n {response.text}")
